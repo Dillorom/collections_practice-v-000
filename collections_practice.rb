d59e899cd27994d009a0175a399d8033a6a7b423
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-02 18:21:25 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+require "pry"
+def sort_array_asc(array)
+  result = array.sort
+  return result
+end
 
+def sort_array_desc(array)
+  result = array.sort {|a, b| b <=> a}
+    return result
+end
+
+def sort_array_char_count(array)
+  result = array.sort {|a, b| a.length <=> b.length}
+  return result
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+    return array
+end
+
+# Advanced
+#def swap_elements_from_to(array, index, destination_index)
+  #result = array.sort {|index, destination_index| destination_index <=> index}
+  #return result
+#end
+
+def reverse_array(array)
+  result = array.reverse
+  return result
+end
+
+def kesha_maker(array)
+  result = array.each do |x|
+    x[2] = "$"
+    end
+end
